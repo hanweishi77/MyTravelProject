f229bf5a2dd8aa19d042519b8070f82f8f30e97f
hanweishi77 <15735181353@163.com>
2023-04-11 17:23:45 +0800

    修改建议表extra_validator 错误。

--- blueprints/forms.py
+++ blueprints/forms.py
@@ -1,25 +1,25 @@
 from flask_wtf import FlaskForm
-import wtforms
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import Email, length, EqualTo, DataRequired
 from models import User
 
 
 class RegisterForm(FlaskForm):
     """用户注册表单"""
-    username = wtforms.StringField(
+    username = StringField(
         "用户名", validators=[DataRequired("用户名不能为空"),
                            length(min=1, max=20, message="用户名长度不符")]
     )
-    email = wtforms.StringField(
+    email = StringField(
         "邮箱", validators=[DataRequired("邮箱不能为空"), Email("邮箱格式错误")]
     )
-    pwd = wtforms.PasswordField(
+    pwd = PasswordField(
         "密码", validators=[DataRequired(), length(min=6, max=20, message="密码格式错误")]
     )
-    re_pwd = wtforms.PasswordField(
+    re_pwd = PasswordField(
         "确认密码", validators=[DataRequired("请确认密码"), EqualTo("pwd", message="两次密码不一致")]
     )
-    submit = wtforms.SubmitField("注册")
+    submit = SubmitField("注册")
 
     def validate(self, extra_validators=None):
         initial_validation = super(RegisterForm, self).validate()
@@ -47,7 +47,7 @@ class LoginForm(FlaskForm):
     """
     登录功能
     """
-    email = wtforms.StringField(
+    email = StringField(
         validators=[
             DataRequired("邮箱不能为空！")
         ],
@@ -57,7 +57,7 @@ class LoginForm(FlaskForm):
             "placeholder": "请输入邮箱！",
         }
     )
-    pwd = wtforms.PasswordField(
+    pwd = PasswordField(
         validators=[
             DataRequired("密码不能为空！")
         ],
@@ -68,7 +68,7 @@ class LoginForm(FlaskForm):
         }
     )
 
-    submit = wtforms.SubmitField(
+    submit = SubmitField(
         '登录',
     )
 
@@ -85,7 +85,7 @@ class LoginForm(FlaskForm):
 
 class SuggetionForm(FlaskForm):
     """意见建议"""
-    name = wtforms.StringField(
+    name = StringField(
         label="姓名",
         validators=[
             DataRequired("姓名不能为空！"), length(min=1, max=20, message="用户名长度不符")
@@ -96,7 +96,7 @@ class SuggetionForm(FlaskForm):
             "class": "form-control"
         }
     )
-    email = wtforms.StringField(
+    email = StringField(
         label="邮箱",
         validators=[
             DataRequired("邮箱不能为空！"), Email("邮箱格式错误")
@@ -107,7 +107,7 @@ class SuggetionForm(FlaskForm):
             "class": "form-control"
         }
     )
-    content = wtforms.TextAreaField(
+    content = TextAreaField(
         label="意见建议",
         validators=[
             DataRequired("内容不能为空！")
@@ -119,9 +119,15 @@ class SuggetionForm(FlaskForm):
             "row": 10
         }
     )
-    submit = wtforms.SubmitField(
+    submit = SubmitField(
         '发送消息',
         render_kw={
             "class": "btn-submit"
         }
     )
+
+    def validate(self, extra_validators=None):
+        initial_validation = super(SuggetionForm, self).validate()
+        if not initial_validation:
+            return False
+        return True
